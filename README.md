f90c3664707474230ac012d3091a4ff7d86ae554
rkJun <juntai81@gmail.com>
2014-08-10 23:30:09 +0900

    ruby script get top 100 mac apps

---
+++ README.md
@@ -0,0 +1,20 @@
+### Get Mac App Store Ranking 
+
+Get App Store's Top Mac Apps, generate ranking files for markdown.
+(Korean, U.S, Japan, U.K, China, Hongkong)
+
+### Dependency
+- ruby
+- ruby gem simple-rss
+  - ```$ gem install simple-rss``` 
+
+### Usage
+```SHELL
+$ ruby ranks.rb
+```
+
+### Result
+- output top 100 mac apps ranking 6 files.
+  (Korea, U.S, U.K, Japan, China, Hongkong)
+
+
---
+++ rank.rb
@@ -0,0 +1,40 @@
+#! /usr/bin/env ruby
+
+require 'simple-rss'
+# require 'rss'
+require 'open-uri'
+
+puts '=== get appstore rank ==='
+
+countries = [ 'kr', 'us', 'jp', 'uk', 'cn', 'hk' ]
+feed_types = [ 'topmacapps', 'topfreemacapps', 'toppaidmacapps' ]
+# 10,25,50,100
+limit = 100
+
+url_prefix = 'https://itunes.apple.com/'
+date = Time.new.strftime("%Y-%m-%d")
+
+countries.each do |country|
+
+  url = "#{url_prefix}/#{country}/rss/#{feed_types[0]}/limit=#{limit}/xml"
+  # rss = RSS::Parser.parse(url)
+  rss = SimpleRSS.parse open(url)
+  # puts rss
+  output = File.open("output_#{country}_#{date}.md", "w")
+
+  puts "Title: #{rss.title}" 
+  output << "### #{rss.title} \n"
+  cnt = 0
+
+  rss.entries.each do |entry|
+    cnt = cnt + 1
+    puts "#{cnt.to_s.rjust(3)} #{entry.title}"
+    output << "#{cnt.to_s}. [#{entry.title}](#{entry.link})\n"
+  end
+
+  output.close
+
+end
+
+
+puts '=== end ==='
